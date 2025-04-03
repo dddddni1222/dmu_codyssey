e1dcefaa6488d326ad54667b212e7ced8b7beb82
dddddni1222 <dddddni_@naver.com>
2025-04-03 20:16:00 +0900

    4주차 과제

--- Week4/mars_mission_computer.py
+++ Week4/mars_mission_computer.py
@@ -17,23 +17,24 @@ class MissionComputer:
     def get_valid_input(self, prompt):
         while True:
             user_input = input(prompt).strip()
-            if user_input.lower() == 'q':
-                self.running = False
-                return None
             try:
                 return float(user_input)
             except ValueError:
                 print("숫자 값을 입력해주세요.")
 
-    def update_env_values(self):
+    def get_sensor_data(self):
         if self.ds:
             self.env_values.update(self.ds.get_env())
+            self.data_history.append(self.env_values.copy())
         else:
             for key in self.env_values.keys():
                 value = self.get_valid_input(f'{key.replace("_", " ").title()} : ')
                 if value is None:
                     return False
                 self.env_values[key] = value
+                self.data_history.append(self.env_values.copy())
+        
+        print(self.format_env_values())
         return True
 
     def format_env_values(self):
@@ -41,7 +42,7 @@ class MissionComputer:
 
     def print_5_min_average(self):
         if not self.data_history:
-            print("\n5-Minute Average Data: No data available yet.")
+            print("\n5-Minute Average Data: 평균 데이터가 없습니다. ")
             return
 
         avg_values = {key: sum(d[key] for d in self.data_history[-60:]) / min(len(self.data_history), 60) for key in self.env_values}
@@ -51,18 +52,15 @@ class MissionComputer:
         last_5_min_check_time = time.time()
 
         while self.running:
-            if not self.update_env_values():
+            if not self.get_sensor_data():
                 print("System stopped....")
                 break
 
-            self.data_history.append(self.env_values.copy())
-            print(self.format_env_values())
-
-            if time.time() - last_5_min_check_time >= 30:
+            if time.time() - last_5_min_check_time >= 10:
                 self.print_5_min_average()
                 last_5_min_check_time = time.time()
 
-            time.sleep(5) 
+            time.sleep(5)
 
 class DummySensor:
     def __init__(self):
@@ -73,12 +71,12 @@ class DummySensor:
         }
 
     def get_env(self):
-        """랜덤 환경 변수 값 반환"""
         return {key: round(random.uniform(*range_), 2) for key, range_ in self.env_ranges.items()}
 
 if __name__ == '__main__':
     try:
         use_dummy = input("더미 센서를 사용할까요? (y/n): ").strip().lower() == 'y'
-        MissionComputer(use_dummy_sensor=use_dummy).run()
+        runComputer = MissionComputer(use_dummy_sensor=use_dummy)
+        runComputer.run()
     except KeyboardInterrupt:
-        print("Sytem stoped….")
+        print("System stopped….")
